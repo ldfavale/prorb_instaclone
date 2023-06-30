1630e13825cc5abe807ee2cc70da163811a0144a
Leonardo Favale <ldfavale@gmail.com>
2023-06-30 16:50:15 -0300

    fix comments on more than one post

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,6 @@
 class SiteController < ApplicationController
   def index
+    redirect_to new_user_session_path, notice: "You need to be logged in to see the posts" unless current_user
     @posts = Post.all
   end
 end
